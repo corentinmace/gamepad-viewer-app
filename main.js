156d7fd4916f2bf50555e88a53573aee4af4bd17
Corentin Mace <corentin.mace@ynov.com>
2021-07-27 12:59:51 +0200

    removed aot

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 const path = require('path')
 const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
 
-function createWindow () {
-    const win = new BrowserWindow({
+app.disableHardwareAcceleration()
+let win
+
+// function createWindow () {
+//     const win = new BrowserWindow({
+//         titleBarStyle: 'show',
+//         icon: 'favicon.ico',
+//         width: 900,
+//         height: 800,
+//         resizable: false,
+//         alwaysOnTop: false,
+//         webPreferences: {
+//             preload: path.join(__dirname, 'preload.js'),
+//             offscreen: true
+//         }
+//     })
+   // win.webContents.openDevTools()
+//}
+
+app.whenReady().then(() => {
+    win = new BrowserWindow({
         titleBarStyle: 'show',
         icon: 'favicon.ico',
         width: 900,
         height: 800,
         resizable: false,
-        alwaysOnTop: true,
+        alwaysOnTop: false,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
         }
     })
    // win.webContents.openDevTools()
     win.loadFile('index.html')
-}
-
-app.whenReady().then(() => {
-    createWindow()
-
     app.on('activate', function() {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
